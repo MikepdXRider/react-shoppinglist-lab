3dfd2103bfb396a1cc6fa631574f35a04d3dfea9
Michael Rider <mikepdxrider@gmail.com>
2021-12-16 19:01:33 -0800

    adds home render test

---
+++ src/views/Home/Home.test.jsx
@@ -0,0 +1,11 @@
+import { render } from '@testing-library/react'
+import Home from "./Home.jsx"
+
+
+it('renders home view component', () => {
+    const {container} = render(
+        <Home />
+    )
+
+    expect(container).toMatchSnapshot();
+})
